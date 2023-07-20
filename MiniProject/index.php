aa8881fe432745bf2fad0c3db8d676fd9ca0b678
PinkWhite <134790534+Brynlai@users.noreply.github.com>
2023-07-20 23:45:01 +0800

    Add files via upload

--- MiniProject/index.php
+++ MiniProject/index.php
@@ -1,1 +1,7 @@
-<?php  include 'inc/dashHeader.php'?>
+<a href="adminSide/pos-panel.php">Admin Panel</a>
+<a href="customerSide/welcome.php">Customer Home Page</a>
+<a href="login.php">Login</a>
+
+
+
+
---
+++ MiniProject/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Water.css -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+
+    <title>Hello, world!</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <form class="col-md-6">
+                <div class="form-group">
+                    <label for="inputEmail3">Email</label>
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword3">Password</label>
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-primary">Sign in</button>
+            </form>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
