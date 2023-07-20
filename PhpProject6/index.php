d8b81e6c9f1fed8e8185b573dabc90d98bf6e401
ahhyang <111368728+ahhyang@users.noreply.github.com>
2023-07-20 23:44:03 +0800

    Add files via upload

---
+++ PhpProject6/index.php
@@ -0,0 +1,377 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>My Website</title>
+</head>
+
+<body>
+  <!-- Header -->
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="#hero">
+            <h1>Shoney's</h1>
+          </a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger">
+            <div class="bar"></div>
+          </div>
+          <ul>
+            <li><a href="#hero" data-after="Home">Home</a></li>
+            <li><a href="#services" data-after="Service">Reservation</a></li>
+            <li><a href="#projects" data-after="Projects">Menu</a></li>
+            <li><a href="#about" data-after="About">About</a></li>
+            <li><a href="#contact" data-after="Contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+
+
+  <!-- Hero Section  -->
+  <section id="hero">
+    <div class="hero container">
+      <div>
+          <h1><strong style="color:goldenrod;">SHONEY'S<span></span></strong></h1>
+          <h1><strong style="color:goldenrod;">DINING & BAR<span></span></strong></h1>
+        <a href="#projects" type="button" class="cta">MENU</a>
+      </div>
+    </div>
+  </section>
+  <!-- End Hero Section  -->
+
+  <!-- Service Section -->
+  <section id="services">
+    <div class="services container">
+      <div class="service-top">
+        <h1 class="section-title">Reser<span>v</span>ation</h1>
+        <p></p>
+      </div>
+      <div class="service-bottom">
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" /></div>
+          <h2>Web Design</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed
+            architecto placeat beatae tenetur officia quod</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" /></div>
+          <h2>Web Design</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed
+            architecto placeat beatae tenetur officia quod</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" /></div>
+          <h2>Web Design</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed
+            architecto placeat beatae tenetur officia quod</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" /></div>
+          <h2>Web Design</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed
+            architecto placeat beatae tenetur officia quod</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Service Section -->
+
+  <!-- Projects Section -->
+  <section id="projects">
+    <div class="projects container">
+      <div class="projects-header">
+        <h1 class="section-title">Me<span>n</span>u</h1>
+      </div>
+      <div class="all-projects">
+          
+        <div class="project-item">
+          <div class="project-info">
+              <h1> <strong> SHONEY'S PLATTER</strong></h1>
+            <h2>﻿RM188</h2>
+            <p>Serves 4-6 persons
+Generous serving of grilled Jumbo Shrimps, Fried Calamari, Chicken & Vegetable Skewers, Southern Fried Chicken, Lamb Chops, Beef Sausage, our famous BBQ Ribs accompanied with Vegetables of the day, Buttered Corn & Cajun Potato Cubes.
+</p>
+          </div>
+          <div class="project-img">
+            <img src="344398852_973575807343422_1569924326631336402_n.jpg" alt="img">
+          </div>
+        </div>
+          
+          
+        <div class="project-item">
+          <div class="project-info">
+            <h1>BAR BITES</h1>
+            <p>Buffalo WingsRM24</p>
+               <p>Fried Calamari RM29</p>
+                <p>Cheesy Baked Mussels   </p>
+                 <p>&ensp; &ensp;&ensp;&ensp;&ensp;&ensp; ½ dozen RM23 </p>
+                 <p> &ensp; &ensp;&ensp;&ensp;&ensp;&ensp; 1 dozen RM38</p>
+                 <p>Chopped Lamb Chopsrm RM39</p>
+                 <p>Nachos RM28</p>
+                 <p>Cheesy FriesRM14</p>
+                 <p>Cheesy Meat FriesRM22</p>
+          </div>
+          <div class="project-img">
+            <img src="344398852_973575807343422_1569924326631336402_n.jpg" alt="img">
+          </div>
+        </div>
+          
+          
+        <div class="project-item">
+          <div class="project-info">
+              <h1>SALAD</h1>
+            
+              <p>Grilled Chicken Caeser's Salad  RM24</p>
+              <p>Shoney's Salad  RM20</p>
+              
+              <h1>SOUP </h1>
+              <p>Soup of the day RM14</p>
+             <p>Classic Cream of Mushroom RM17</p>
+            <p>Seafood Tomato, RM24</p>
+            <p>Seafood Chowder RM24</p>
+          </div>
+          <div class="project-img">
+             <img src="344398852_973575807343422_1569924326631336402_n.jpg" alt="img">
+          </div>
+        </div>
+          
+          
+        <div class="project-item">
+          <div class="project-info">
+            <h1>STEAK & RIB</h1>
+            
+            <p>Prime Rib Steak (300g)  RM98</p>
+    <p>Sirloin Steak (230g)	RM79</p>
+    <p>Ribeye Steak (230g)	RM96</p>
+    <p>BBQ Ribs 	RM59</p>
+          </div>
+          <div class="project-img">
+             <img src="344398852_973575807343422_1569924326631336402_n.jpg" alt="img">
+          </div>
+        </div>
+          
+          
+        <div class="project-item">
+          <div class="project-info">
+            <h1>CHICKEN</h1>
+           
+        <p>Grilled Chicken ½  Bird RM33</p>
+        <p>Souther Fried Chicken ½  Bird RM33</p> 
+        <p>Pan Seared Chicken RM28</p>
+        <p>Chicken Chop RM28</p>
+          </div>
+          <div class="project-img">
+            <img src="344398852_973575807343422_1569924326631336402_n.jpg" alt="img">
+          </div>
+        </div>
+          
+            <div class="project-item">
+          <div class="project-info">
+            <h1>LAMB</h1>  
+         <p>Grilled Lamb Chop RM48</p>
+         <p>Braised Lamb Shank RM52</p>
+  
+          </div>
+          <div class="project-img">
+             <img src="344398852_973575807343422_1569924326631336402_n.jpg" alt="img">
+          </div>
+        </div>
+          
+            <div class="project-item">
+          <div class="project-info">
+            <h1>SEAFOOD</h1>
+            
+            <p>Catch of the day RM46</p>
+    <p>Grilled Salmon RM48</p>
+    <p>Jambalaya RM28</p>
+    <p>Fish & Chips RM35</p>
+          </div>
+          <div class="project-img">
+             <img src="344398852_973575807343422_1569924326631336402_n.jpg" alt="img">
+          </div>
+        </div>
+          
+          
+          
+            <div class="project-item">
+          <div class="project-info">
+            <h1>BURGERS & SANDWICHES</h1>
+    <p>Classic Cheese Burger RM30</p>
+    <p>Hickory Burger RM30 </p>
+    <p>Fried Chicken Burger RM24</p>
+    <p>Gelled Chicken Burger RM24</p>
+    <p>ChitDog RM25</p>
+    <p>Meatballs Sandwich RM25</p>
+    <p>Street Car RM24</p>
+    <p>Shrimp Po Boy RM32</p>
+    <p>Chichen Po Boy RM28</p>
+    <p>Chicken Tortilla RM22</p>
+
+          </div>
+          <div class="project-img">
+             <img src="344398852_973575807343422_1569924326631336402_n.jpg" alt="img">
+          </div>
+        </div>
+          
+          
+            <div class="project-item">
+          <div class="project-info">
+            <h1>PASTA</h1>
+      <p> Bolognese RM26  <p>
+      <p>Meat Balls RM28<p>
+      <p> Carbonan RM28<p>
+      <p> Chicken & Mushroom Aglio Olio RM28<p>
+      <p>Chicken Arabiatta RM28<p>
+      <p>Seafood Agiko Olio RM32<p>
+      <p>Shrimp marinara RM32<p>
+      <p>Chef's Signature pasta<p> 
+      <p>Italian Chicken RM30<p>
+      <p>Braised Lamb Cutiet RM33<p>
+
+          </div>
+          <div class="project-img">
+             <img src="344398852_973575807343422_1569924326631336402_n.jpg" alt="img">
+          </div>
+        </div>
+          
+          
+            <div class="project-item">
+          <div class="project-info">
+            <h1>SIDE DISHES</h1>
+            <p> Fries RM9 </p>
+<p>Potato Wedges RM9</p>
+<p>Garden Salad RM9</p>
+<p>Garlic Mashed Potato RM9</p>
+<p>V.O.D (Vegetables of the day) RM9</p>
+<p>Wan Tan Fritters RM9</p>
+<p>Buttered Corn RM9</p>
+<p>Coleslaw RM9</p>
+<p>Garlic Bread RM9</p>
+    <p>Dirty Rice RM9</p>
+
+          </div>
+          <div class="project-img">
+             <img src="344398852_973575807343422_1569924326631336402_n.jpg" alt="img">
+          </div>
+        </div>
+          
+      </div>
+    </div>
+  </section>
+  <!-- End Projects Section -->
+
+  <!-- About Section -->
+  <section id="about">
+    <div class="about container">
+      <div class="col-left">
+        <div class="about-img">
+         <img src="shoney's logo.png" alt=""/>
+        </div>
+      </div>
+      <div class="col-right">
+        <h1 class="section-title">About <span>Us</span></h1>
+        <h2>SHONEY'S DINING & BAR Company History:</h2>
+ <p>Shoney's Dining & Bar is a well-established Western food establishment in the city's heart. Shoney's Dining & Bar has become a popular choice for customers looking to celebrate special occasions or simply enjoy a relaxing meal, with a focus on providing delicious meals and a friendly dining experience.
+ </p>
+ <p>Shoney's Dining & Bar, as a Western restaurant, offers a diverse menu that caters to a variety of tastes. The menu includes a wide range of options such as bar bites, salads, soups and a variety of main courses. Customers can savour succulent options such as steak and ribs, chicken, lamb, seafood, burgers and sandwiches, pasta, and a variety of delectable side dishes. The menu has been carefully curated to offer a balance of classic favourites and innovative creations, ensuring that every palate is satisfied.
+ </p>
+ <p>Shoney's Dining & Bar's ability to accommodate customers is one of its distinguishing features. Shoney's Dining & Bar strives to create an inviting and comfortable dining environment, whether guests prefer to walk in or make reservations in advance. The restaurant recognises the significance of creating memorable experiences, particularly for those celebrating special occasions. Shoney's Dining & Bar is a popular choice for families, couples, and groups of friends because of its attentive staff and welcoming atmosphere.
+ </p>
+ <p>Shoney's Dining & Bar has an inviting outdoor bar that is open seven days a week from 11:00 AM to 10:00 PM in addition to the indoor dining area.This outdoor space provides a relaxed setting for patrons to unwind and socialise while sipping on their favourite drinks and nibbling on bar bites. The bar serves a wide range of beverages, including cocktails, wines, beers and non-alcoholic options.
+ </p>
+     <a href="#" class="cta">Download Resume</a>
+      </div>
+    </div>
+  </section>
+  <!-- End About Section -->
+
+  
+  
+  
+  
+  
+  
+  <!-- Contact Section -->
+  <section id="contact">
+    <div class="contact container">
+      <div>
+        <h1 class="section-title">Contact <span>info</span></h1>
+      </div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <i class="fa-sharp fa-solid fa-phone fa-beat"></i>
+          <div class="contact-info">
+              <i class="fa-sharp fa-solid fa-phone fa-beat"></i>
+              <div class='icon'><img src="icons8-phone-100.png" alt=""/></div>
+            <h1>Phone</h1>
+            <h2>+60 886 8786</h2>
+          </div>
+        </div>
+          
+          
+          
+        <div class="contact-item"> 
+          <div class="contact-info">
+              <div class='icon'><img src="icons8-email-100.png" alt=""/></div>
+            <h1>Email</h1>
+            <h2>shoney's_Bar&Grill@gmail.com</h2> 
+          </div>
+        </div>
+          
+          
+        <div class="contact-item">
+            <div class="contact-info">
+                <div class='icon'> <img src="icons8-home-address-100.png" alt=""/></div>
+                
+               
+            <h1>Address</h1>
+            <h2>Lot 52, Ground Floor, Jalan Gaya, No.1, Lorong Ewan, Kota Kinabalu, Malaysia, 88000</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact Section -->
+
+  
+  
+  
+  
+  <!-- Footer -->
+  <section id="footer">
+    <div class="footer container">
+      <div class="brand">
+          <h1>SHONEY'S DINING & BAR</h1>
+      </div>
+      <h2>Your Complete Web Solution</h2>
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/twitter.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/behance.png" /></a>
+        </div>
+      </div>
+      <p>Copyright © 2023 SHONEY's. All rights reserved</p>
+    </div>
+  </section>
+  <!-- End Footer -->
+  <script src="./app.js"></script>
+</body>
+
+</html>
