abe22685c7d62dca3bfdfa0ae8cce1eb6101c284
PinkWhite <134790534+Brynlai@users.noreply.github.com>
2023-07-21 11:21:41 +0800

    Add files via upload

---
+++ MiniProject/adminSide/menuCrud/createItem.php
@@ -0,0 +1,105 @@
+<?php  include 'inc/dashHeader.php'?>
+<?php
+// Include config file
+require_once "../config.php";
+ 
+// Define variables and initialize with empty values
+$name = $address = $salary = "";
+$name_err = $address_err = $salary_err = "";
+$item_id = $item_name = $item_type = $item_category = $item_price = $item_description = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Validate name
+    $input_name = trim($_POST["name"]);
+    if(empty($input_name)){
+        $name_err = "Please enter a name.";
+    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $name_err = "Please enter a valid name.";
+    } else{
+        $name = $input_name;
+    }
+    
+    // Validate address
+    $input_address = trim($_POST["address"]);
+    if(empty($input_address)){
+        $address_err = "Please enter an address.";     
+    } else{
+        $address = $input_address;
+    }
+    
+    // Validate salary
+    $input_salary = trim($_POST["salary"]);
+    if(empty($input_salary)){
+        $salary_err = "Please enter the salary amount.";     
+    } elseif(!ctype_digit($input_salary)){
+        $salary_err = "Please enter a positive integer value.";
+    } else{
+        $salary = $input_salary;
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($name_err) && empty($address_err) && empty($salary_err)){
+        // Prepare an insert statement
+        $sql = "INSERT INTO employees (name, address, salary) VALUES (?, ?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "sss", $param_name, $param_address, $param_salary);
+            
+            // Set parameters
+            $param_name = $name;
+            $param_address = $address;
+            $param_salary = $salary;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records created successfully. Redirect to landing page
+                header("location: index.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+ 
+
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5">Create Record</h2>
+                    <p>Please fill this form and submit to add employee record to the database.</p>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>">
+                            <span class="invalid-feedback"><?php echo $name_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Address</label>
+                            <textarea name="address" class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>"><?php echo $address; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $address_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Salary</label>
+                            <input type="text" name="salary" class="form-control <?php echo (!empty($salary_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $salary; ?>">
+                            <span class="invalid-feedback"><?php echo $salary_err;?></span>
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+    
+<?php  include '../../inc/dashFooter.php'?>
