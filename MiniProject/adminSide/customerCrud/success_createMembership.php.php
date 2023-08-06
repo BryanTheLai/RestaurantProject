bfb9b32e8e98e0cd49435e88282eeadb578f9455
PinkWhite <134790534+Brynlai@users.noreply.github.com>
2023-08-06 10:29:22 +0800

    Update and rename success_createCust.php to success_createMembership.php.php

--- MiniProject/adminSide/customerCrud/success_createMembership.php.php
+++ MiniProject/adminSide/customerCrud/success_createMembership.php.php
@@ -16,38 +16,34 @@ if ($conn->connect_error) {
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the values from the form
-    $item_id = $_POST["item_id"];
-    $item_name = $_POST["item_name"];
-    $item_type = $_POST["item_type"];
-    $item_category = $_POST["item_category"];
-    $item_price = $_POST["item_price"];
-    $item_description = $_POST["item_description"];
+    $member_name = $_POST["member_name"];
+    $points = $_POST["points"];
 
-    // Prepare the SQL query to check if the item_id already exists
-    $check_query = "SELECT item_id FROM Items WHERE item_id = ?";
+    // Prepare the SQL query to check if the member_name already exists
+    $check_query = "SELECT member_name FROM Memberships WHERE member_name = ?";
     $check_stmt = $conn->prepare($check_query);
-    $check_stmt->bind_param("s", $item_id);
+    $check_stmt->bind_param("s", $member_name);
     $check_stmt->execute();
     $check_result = $check_stmt->get_result();
 
-    // Check if the item_id already exists
+    // Check if the member_name already exists
     if ($check_result->num_rows > 0) {
-        $message = "The item_id is already in use.<br>Please try again to choose a different item_id.";
+        $message = "The member name is already in use.<br>Please try again to choose a different member name.";
         $iconClass = "fa-times-circle";
         $cardClass = "alert-danger";
         $bgColor = "#FFA7A7"; // Custom background color for error
     } else {
         // Prepare the SQL query for insertion
-        $insert_query = "INSERT INTO Items (item_id, item_name, item_type, item_category, item_price, item_description) 
-                        VALUES (?, ?, ?, ?, ?, ?)";
+        $insert_query = "INSERT INTO Memberships (member_name, points) 
+                        VALUES (?, ?)";
         $stmt = $conn->prepare($insert_query);
 
         // Bind the parameters
-        $stmt->bind_param("ssssds", $item_id, $item_name, $item_type, $item_category, $item_price, $item_description);
+        $stmt->bind_param("si", $member_name, $points);
 
         // Execute the query
         if ($stmt->execute()) {
-            $message = "Item created successfully.";
+            $message = "Membership created successfully.";
             $iconClass = "fa-check-circle";
             $cardClass = "alert-success";
             $bgColor = "#D4F4DD"; // Custom background color for success
