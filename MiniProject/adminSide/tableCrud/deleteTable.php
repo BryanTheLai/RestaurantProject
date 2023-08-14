8333a9fee7000fc3cc34c5a0297c8391077badde
kevin07212004 <139090090+kevin07212004@users.noreply.github.com>
2023-08-14 11:36:36 +0800

    14 August

--- MiniProject/adminSide/tableCrud/deleteTable.php
+++ MiniProject/adminSide/tableCrud/deleteTable.php
@@ -2,22 +2,37 @@
 // Include config file
 require_once "../config.php";
 
-// Check if the item_id parameter is set in the URL
+// Check if the table_id parameter is set in the URL
 if (isset($_GET['id'])) {
-    // Get the item_id from the URL and sanitize it
-    $item_id = intval($_GET['id']);
+    // Get the table_id from the URL and sanitize it
+    $table_id = intval($_GET['id']);
 
-    // Construct the DELETE query
-    $deleteSQL = "DELETE FROM items WHERE item_id = '" . $_GET['id'] . "';";
+    // Remove or update related records in referencing table(s)
+    // For example, assuming a referencing table named 'Reservations'
+    $deleteReservationsSQL = "DELETE FROM Reservations WHERE table_id = ?";
+    if ($stmt = mysqli_prepare($link, $deleteReservationsSQL)) {
+        mysqli_stmt_bind_param($stmt, "i", $table_id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+    }
 
-    // Execute the DELETE query
-    if (mysqli_query($link, $deleteSQL)) {
-        // Item successfully deleted, redirect back to the main page
-        header("location: ../panel/table-panel.php");
-        
-        exit();
+    // Construct the DELETE query with a parameterized query
+    $deleteSQL = "DELETE FROM Restaurant_Tables WHERE table_id = ?";
+
+    // Prepare and execute the DELETE query
+    if ($stmt = mysqli_prepare($link, $deleteSQL)) {
+        mysqli_stmt_bind_param($stmt, "i", $table_id);
+        if (mysqli_stmt_execute($stmt)) {
+            // Table successfully deleted, redirect back to the main page
+            header("location: ../panel/table-panel.php");
+            exit();
+        } else {
+            // Error occurred during execution, display an error message
+            echo "Error: " . mysqli_stmt_error($stmt);
+        }
+        mysqli_stmt_close($stmt);
     } else {
-        // Error occurred during execution, display an error message
+        // Error occurred while preparing the statement
         echo "Error: " . mysqli_error($link);
     }
 
