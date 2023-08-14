1129fcfcdf8d661f8816ac64d8282f3c873fc6a3
PinkWhite <134790534+Brynlai@users.noreply.github.com>
2023-08-14 10:49:58 +0800

    Add files via upload

---
+++ MiniProject/inc/dashHeader.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Dashboard - SB Admin</title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="../css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <!-- Sidebar Toggle-->
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <!-- Navbar Brand-->
+            <a class="navbar-brand ps-3" href="../panel/pos-panel.php">Johnny's Staff Panel</a>
+            
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Main</div>
+                            <a class="nav-link" href="../panel/pos-panel.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-cash-register"></i></div>
+                                Point of Sale 
+                            </a>
+                            <a class="nav-link" href="../panel/table-panel.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table-cells"></i></div>
+                                Table
+                            </a>
+                            <a class="nav-link" href="../panel/menu-panel.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-utensils"></i></div>
+                                Menu
+                            </a>
+                            <a class="nav-link" href="../panel/reservation-panel.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-book"></i></div>
+                                Reservations
+                            </a>
+                            <a class="nav-link" href="../panel/customer-panel.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-eye"></i></div>
+                                Members
+                            </a>
+                            <a class="nav-link" href="../panel/staff-panel.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-people-roof"></i></div>
+                                Staff
+                            </a>
+                            <a class="nav-link" href="../panel/account-panel.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-eye"></i></div>
+                                Online Accounts
+                            </a>
+                            <div class="sb-sidenav-menu-heading">Reports</div>
+                            <a class="nav-link" href="../panel/sales-panel.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                                Sales
+                            </a>
+                            <a class="nav-link" href="../panel/statistics-panel.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-fire"></i></div>
+                                Statistics
+                            </a>
+                            
+                            <a class="nav-link" href="../panel/profile-panel.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-user"></i></div>
+                                Profile
+                            </a>
+                            
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                        Put Staff Id and name
+                    </div>
+                </nav>
+            </div>
+        </<div>
+            <div id="content-for-template">Content</div> 
+        
+        <script src="../js/scripts.js" type="text/javascript"></script>
