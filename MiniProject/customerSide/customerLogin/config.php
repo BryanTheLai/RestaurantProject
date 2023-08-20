c6c86fa93ec30132b0805d349df60c3696b49bec
yong <111368728+ahhyang@users.noreply.github.com>
2023-08-20 11:18:00 +0800

    Add files via upload
    new cutomerr login  username , password , comfirm passwrd , email , phone num .

--- MiniProject/customerSide/customerLogin/config.php
+++ MiniProject/customerSide/customerLogin/config.php
@@ -1,16 +1,12 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'mysqli_connect');
- 
-/* Attempt to connect to MySQL database */
+define('DB_NAME', 'restaurantdb'); // Updated database name
+
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-// Check connection
-if($link === false){
+
+if ($link === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
