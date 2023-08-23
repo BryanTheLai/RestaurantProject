4e0ec95e56e11d230350ea38f3fd63f37978dfac
PinkWhite <134790534+Brynlai@users.noreply.github.com>
2023-08-23 23:13:47 +0800

    Update pos-panel.php

--- MiniProject/adminSide/panel/pos-panel.php
+++ MiniProject/adminSide/panel/pos-panel.php
@@ -1,1 +1,21 @@
-<?php  include '../inc/dashHeader.php'?>
+<?php
+include '../inc/dashHeader.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Your head content here -->
+</head>
+<body>
+<div class="container">
+    <div id="POS-Content" class="row center-middle">
+            <a href="reservation-panel.php" class="btn btn-dark btn-lg" title="View Details">Reservation</a>
+            <a href="orderItem.php" class="btn btn-dark btn-lg" title="View Details">Order</a>
+    </div>
+</div>
+
+<!-- Include footer -->
+<?php include '../inc/dashFooter.php' ?>
+</body>
+</html>
