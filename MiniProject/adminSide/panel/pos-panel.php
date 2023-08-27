b926ad12e38874587df797c4840e03820d002c36
PinkWhite <134790534+Brynlai@users.noreply.github.com>
2023-08-27 16:46:29 +0800

    Add files via upload

--- MiniProject/adminSide/panel/pos-panel.php
+++ MiniProject/adminSide/panel/pos-panel.php
@@ -5,13 +5,13 @@ include '../inc/dashHeader.php';
 <!DOCTYPE html>
 <html>
 <head>
-    <!-- Your head content here -->
+    <link href="../css/pos.css" rel="stylesheet" />
 </head>
 <body>
 <div class="container">
     <div id="POS-Content" class="row center-middle">
             <a href="reservation-panel.php" class="btn btn-dark btn-lg" title="View Details">Reservation</a>
-            <a href="orderItem.php" class="btn btn-dark btn-lg" title="View Details">Order</a>
+            <a href="../posBackend/posTable.php" class="btn btn-dark btn-lg" title="View Details">Order</a>
     </div>
 </div>
 
