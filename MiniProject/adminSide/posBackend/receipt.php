2c64c9d3024435785a0b4b0fd69e5b6c6cde8ea5
PinkWhite <134790534+Brynlai@users.noreply.github.com>
2023-08-31 16:46:26 +0800

    Update receipt.php

--- MiniProject/adminSide/posBackend/receipt.php
+++ MiniProject/adminSide/posBackend/receipt.php
@@ -91,16 +91,16 @@ $tax_amount = $before_tax * $tax_rate;
 $after_tax = $before_tax + $tax_amount;
 
 // Totals
-$pdf->Cell(120, 10, 'Before Tax', 1);
-$pdf->Cell(70, 10, 'RM ' . $before_tax, 1);
+$pdf->Cell(150, 10, 'Before Tax', 1);
+$pdf->Cell(40, 10, 'RM ' . $before_tax, 1);
 $pdf->Ln();
 
-$pdf->Cell(120, 10, 'Tax (' . ($tax_rate * 100) . '%)', 1);
-$pdf->Cell(70, 10, 'RM ' . $tax_amount, 1);
+$pdf->Cell(150, 10, 'Tax (' . ($tax_rate * 100) . '%)', 1);
+$pdf->Cell(40, 10, 'RM ' . $tax_amount, 1);
 $pdf->Ln();
 
-$pdf->Cell(120, 10, 'After Tax', 1);
-$pdf->Cell(70, 10, 'RM ' . $after_tax, 1);
+$pdf->Cell(150, 10, 'After Tax', 1);
+$pdf->Cell(40, 10, 'RM ' . $after_tax, 1);
 $pdf->Ln();
 
 $pdf->Output();
