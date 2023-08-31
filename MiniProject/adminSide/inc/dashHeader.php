e9092e1b9134316cba167872a92396a9729de8b7
PinkWhite <134790534+Brynlai@users.noreply.github.com>
2023-08-31 16:54:39 +0800

    Update dashHeader.php

--- MiniProject/adminSide/inc/dashHeader.php
+++ MiniProject/adminSide/inc/dashHeader.php
@@ -7,9 +7,13 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Dashboard - SB Admin</title>
-        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
         <link href="../css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> 
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
     </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
@@ -57,20 +61,17 @@
                                 <div class="sb-nav-link-icon"><i class="fas fa-kitchen-set"></i></div>
                                 Kitchen
                             </a>
-                            <div class="sb-sidenav-menu-heading">Reports</div>
+                            <div class="sb-sidenav-menu-heading">Report & Analytics</div>
                             <a class="nav-link" href="../panel/sales-panel.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                                Sales
+                                <div class="sb-nav-link-icon"><i class="fas fa-fire"></i></div>
+                                Items
                             </a>
                             <a class="nav-link" href="../panel/statistics-panel.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-fire"></i></div>
-                                Statistics
+                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                                Revenue
                             </a>
                             
-                            <a class="nav-link" href="../panel/profile-panel.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-user"></i></div>
-                                Profile
-                            </a>
+                            
                             
                         </div>
                     </div>
