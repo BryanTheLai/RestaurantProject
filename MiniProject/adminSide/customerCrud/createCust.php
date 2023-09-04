bfbf41fdc23bb534b9b36a685bdd6d02efef233e
kevin07212004 <139090090+kevin07212004@users.noreply.github.com>
2023-09-04 10:18:59 +0800

    4 September

--- MiniProject/adminSide/customerCrud/createCust.php
+++ MiniProject/adminSide/customerCrud/createCust.php
@@ -6,28 +6,6 @@ require_once "../config.php";
 $input_member_name = $member_name_err = $member_name = "";
 $input_points = $points_err = $points = "";
 
-// Processing form data when form is submitted
-if(isset($_POST['submit'])){
-    if (empty($_POST['member_name'])) {
-        $member_name_err = 'Member name is required';
-    } else {
-        $member_name = filter_input(
-            INPUT_POST,
-            'member_name',
-            FILTER_SANITIZE_FULL_SPECIAL_CHARS
-        );
-    }
-
-    if (empty($_POST['points'])) {
-        $points_err = 'Points are required';
-    } else {
-        $points = filter_input(
-            INPUT_POST,
-            'points',
-            FILTER_SANITIZE_NUMBER_INT
-        );
-    }
-}
 ?>
 <head>
     <meta charset="UTF-8">
@@ -45,9 +23,18 @@ if(isset($_POST['submit'])){
     <p>Please fill in Membership Information Properly</p>
 
     <form method="POST" action="success_createMembership.php" class="ht-600 w-50">
+        
+        <div class="form-group">
+            <label for="member_id" class="form-label">Member ID:</label>
+            <input type="number" name="member_id" placeholder="99" class="form-control <?php echo !$member_idErr ?: 'is-invalid'; ?>" id="member_id" required value="<?php echo $member_id; ?>"><br>
+            <div id="validationServerFeedback" class="invalid-feedback">
+                Please provide a valid member_id.
+            </div>
+        </div>
+        
         <div class="form-group">
             <label for="member_name" class="form-label">Member Name :</label>
-            <input type="text" name="member_name" class="form-control <?php echo !$member_name_err ? 'is-invalid' : ''; ?>" id="member_name" required placeholder="John Doe" value="<?php echo $member_name; ?>"><br>
+            <input type="text" name="member_name" placeholder="Johnny" class="form-control <?php echo !$member_name_err ?: 'is-invalid' ; ?>" id="member_name" required value="<?php echo $member_name; ?>"><br>
             <div id="validationServerFeedback" class="invalid-feedback">
                 Please provide a valid member name.
             </div>
@@ -55,12 +42,20 @@ if(isset($_POST['submit'])){
 
         <div class="form-group">
             <label for="points">Points :</label>
-            <input type="number" name="points" id="points" required class="form-control <?php echo !$points_err ? 'is-invalid' : ''; ?>" value="<?php echo $points; ?>"><br>
+            <input type="number" name="points" id="points" required class="form-control <?php echo !$points_err ?: 'is-invalid' ; ?>" value="<?php echo $points; ?>"><br>
             <div id="validationServerFeedback" class="invalid-feedback">
                 Please provide valid points.
             </div>
         </div>
 
+        <div class="form-group">
+            <label for="account_id" class="form-label">Account ID:</label>
+            <input type="number" name="account_id" placeholder="99" class="form-control <?php echo !$account_idErr ?: 'is-invalid'; ?>" id="account_id" required value="<?php echo $account_id; ?>"><br>
+            <div id="validationServerFeedback" class="invalid-feedback">
+                Please provide a valid account_id.
+            </div>
+        </div>
+        
         <div class="form-group">
             <input type="submit" name="submit" class="btn btn-primary" value="Create Membership">
         </div>
