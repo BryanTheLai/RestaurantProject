f0df1ad30544bb46db54ad97f18d192dcbd6975f
kevin07212004 <139090090+kevin07212004@users.noreply.github.com>
2023-09-12 13:52:01 +0800

    12 September

--- MiniProject/adminSide/reservationsCrud/insertReservation.php
+++ MiniProject/adminSide/reservationsCrud/insertReservation.php
@@ -1,4 +1,42 @@
-?php
+<?php
+// reservation.php
+require_once '../config.php';
+session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the values from the form
+    $customer_name = $_POST["customer_name"];
+    $table_id = intval($_POST["table_id"]);
+    $reservation_time = $_POST["reservation_time"];
+    $reservation_date = $_POST["reservation_date"];
+    $special_request = $_POST["special_request"];
+    
+    $select_query_capacity = "SELECT capacity FROM restaurant_tables WHERE table_id='$table_id';";
+    $results_capacity = mysqli_query($link, $select_query_capacity);
+
+    if ($results_capacity) {
+        $row = mysqli_fetch_assoc($results_capacity);
+        $head_count = $row['capacity'];
+
+        $reservation_id = $customer_name . "_" . $reservation_date . "_" . $head_count;
+
+        // Prepare the SQL query for insertion
+        $insert_query1 = "INSERT INTO Reservations (reservation_id, customer_name, table_id, reservation_time, reservation_date, head_count, special_request) 
+                        VALUES ('$reservation_id', '$customer_name', '$table_id', '$reservation_time', '$reservation_date', '$head_count', '$special_request');";
+        $insert_query2 = "INSERT INTO Table_Availability (availability_id, table_id, reservation_date, reservation_time, status) 
+                        VALUES ('$reservation_id', '$table_id', '$reservation_date', '$reservation_time',  'no');";
+        mysqli_query($link, $insert_query1);
+        mysqli_query($link, $insert_query2);
+
+        $_SESSION['customer_name'] = $customer_name;
+        //header("Location: createReservation.php?reservation=success");
+    } else {
+        // Handle the case where the query failed
+        echo "Error fetching table capacity: " . mysqli_error($link);
+    }
+}
+?>
+
+<?php
 // Define the variables
 $iconClass = 'fa-check-circle'; // This value indicates success; you can adjust it as needed
 $cardClass = 'alert-success';   // This value indicates a success message card; adjust as needed
