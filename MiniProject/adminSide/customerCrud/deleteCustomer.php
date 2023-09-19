48d78a07eaa3c91f63ac75456201e449d3aa57cd
PinkWhite <134790534+Brynlai@users.noreply.github.com>
2023-09-19 21:24:06 +0800

    Add files via upload

--- MiniProject/adminSide/customerCrud/deleteCustomer.php
+++ MiniProject/adminSide/customerCrud/deleteCustomer.php
@@ -7,18 +7,33 @@ if (isset($_GET['id'])) {
     // Get the member_id from the URL and sanitize it
     $member_id = intval($_GET['id']);
 
+    // Disable foreign key checks
+    $disableForeignKeySQL = "SET FOREIGN_KEY_CHECKS=0;";
+    mysqli_query($link, $disableForeignKeySQL);
+
     // Construct the DELETE query
-    $deleteSQL = "DELETE FROM Memberships WHERE member_id = $member_id";
+    $deleteSQL = "DELETE FROM Memberships WHERE member_id = ?";
+
+    // Prepare the DELETE query
+    $stmt = $link->prepare($deleteSQL);
+    $stmt->bind_param("i", $member_id);
 
     // Execute the DELETE query
-    if (mysqli_query($link, $deleteSQL)) {
+    if ($stmt->execute()) {
         // Membership successfully deleted, redirect back to the main page
         header("location: ../panel/customer-panel.php");
         exit();
     } else {
         // Error occurred during execution, display an error message
-        echo "Error: " . mysqli_error($link);
+        echo "Error: " . $stmt->error;
     }
+
+    // Enable foreign key checks
+    $enableForeignKeySQL = "SET FOREIGN_KEY_CHECKS=1;";
+    mysqli_query($link, $enableForeignKeySQL);
+
+    // Close the statement
+    $stmt->close();
 
     // Close the connection
     mysqli_close($link);
