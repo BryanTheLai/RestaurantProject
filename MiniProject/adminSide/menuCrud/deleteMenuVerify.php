9970cedf4b5314405b7291d992b79cc3722e30cb
PinkWhite <134790534+Brynlai@users.noreply.github.com>
2023-09-19 10:34:59 +0800

    Add files via upload

---
+++ MiniProject/adminSide/menuCrud/deleteMenuVerify.php
@@ -0,0 +1,136 @@
+<?php
+require_once "../config.php";
+
+// Check if 'id' is set and not empty
+if (isset($_GET['id']) && !empty($_GET['id'])) {
+    $menu_id = $_GET['id'];
+} else {
+    header("Location: ../panel/menu-panel.php");
+    exit(); // Make sure to exit after redirect
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // User-provided input
+    $provided_account_id = $_POST['admin_id']; // 99999
+    $provided_password = $_POST['password']; // 12345
+    $uniqueString = $provided_account_id . $provided_password;
+    echo 'uniqueString: ' . $uniqueString;
+
+    if ($uniqueString == "9999912345") {
+        echo ' Correct';
+        header("Location: ../menuCrud/deleteItem.php?id=".$menu_id ."");
+    } else {
+        echo ' WRONG';
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        /* Add your custom styles here */
+        body {
+            font-family: 'Montserrat', sans-serif;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0; /* Remove default margin */
+            background-color: black;
+            background-image: url('../image/loginBackground.jpg'); /* Set the background image path */
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            color: white;
+        }
+
+        .login-container {
+            padding: 50px; /* Adjust the padding as needed */
+            border-radius: 10px; /* Add rounded corners */
+            margin: 100px auto; /* Center the container horizontally */
+            max-width: 500px; /* Set a maximum width for the container */
+        }
+
+        .login_wrapper {
+            width: 400px; /* Adjust the container width as needed */
+            padding: 20px;
+        }
+
+        h2 {
+            text-align: center;
+            font-family: 'Georgia', serif;
+        }
+
+        p {
+            font-family: 'Georgia', serif;
+        }
+
+        .form-group {
+            margin-bottom: 15px; /* Add space between form elements */
+        }
+
+        ::placeholder {
+            font-size: 12px; /* Adjust the font size as needed */
+        }
+
+        button, select {
+            background-color: black;
+            color: white;
+            border: 2px solid black;
+            padding: 3px 10px;
+            border-radius: 5px;
+            cursor: pointer;
+            display: inline-block;
+            touch-action: manipulation;
+            font-family: serif;
+            border-color: #41403e;
+            height: 40px;
+            width: 66px;
+            box-shadow: rgba(0, 0, 0, .2) 15px 28px 25px -18px;
+            transition: background-color 0.3s, color 0.3s, border 0.3s;
+        }
+
+        /* Style buttons and selects on hover */
+        button:hover, select:hover {
+            background-color: white;
+            color: black;
+            border: 2px solid black;
+            box-shadow: rgba(0, 0, 0, .3) 2px 8px 8px -5px;
+            transform: translate3d(0, 2px, 0);
+        }
+
+        .button:focus {
+            box-shadow: rgba(0, 0, 0, .3) 2px 8px 4px -6px;
+        }
+    </style>
+</head>
+<body>
+    <div class="login-container">
+        <div class="login_wrapper">
+            <div class="wrapper">
+                <h2>Delete Menu Item</h2>
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label>Admin Id</label>
+                        <input type="number" name="admin_id" class="form-control" placeholder="Enter User Email" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control" placeholder="Enter User Password" required>
+                    </div>
+
+                    <button type="submit" name="submit" value="submit">Delete</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- MiniProject/adminSide/tableCrud/deleteTable.php
+++ MiniProject/adminSide/tableCrud/deleteTable.php
@@ -7,14 +7,10 @@ if (isset($_GET['id'])) {
     // Get the table_id from the URL and sanitize it
     $table_id = intval($_GET['id']);
 
-    // Remove or update related records in referencing table(s)
-    // For example, assuming a referencing table named 'Reservations'
-    $deleteReservationsSQL = "DELETE FROM Reservations WHERE table_id = ?";
-    if ($stmt = mysqli_prepare($link, $deleteReservationsSQL)) {
-        mysqli_stmt_bind_param($stmt, "i", $table_id);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_close($stmt);
-    }
+    // Disable foreign key checks
+    $disableForeignKeySQL = "SET FOREIGN_KEY_CHECKS = 0";
+    mysqli_query($link, $disableForeignKeySQL);
+
 
     // Construct the DELETE query with a parameterized query
     $deleteSQL = "DELETE FROM Restaurant_Tables WHERE table_id = ?";
@@ -35,6 +31,10 @@ if (isset($_GET['id'])) {
         // Error occurred while preparing the statement
         echo "Error: " . mysqli_error($link);
     }
+
+    // Re-enable foreign key checks
+    $enableForeignKeySQL = "SET FOREIGN_KEY_CHECKS = 1";
+    mysqli_query($link, $enableForeignKeySQL);
 
     // Close the connection
     mysqli_close($link);
