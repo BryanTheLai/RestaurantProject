d7106d5628bb3438bce2182e5eb5b54ce7cef0bd
yong <111368728+ahhyang@users.noreply.github.com>
2023-09-23 01:57:24 +0800

    Add files via upload
    logout wif no noti

--- MiniProject/customerSide/customerLogin/logout.php
+++ MiniProject/customerSide/customerLogin/logout.php
@@ -42,37 +42,9 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
     </style>
 </head>
 <body>
-    <div class="container">
-        <!-- Replace the alert box div with PHP code -->
-        <?php echo '
-        <div class="alert-box" style="float: none; margin: 0 auto;">
-            <div class="alert alert-success">
-                <div class="alert-icon text-center">
-                    <i class="fa fa-check-square-o fa-3x" aria-hidden="true"></i>
-                </div>
-                <div class="alert-message text-center">
-                    <strong>Success!</strong> Thank you for signing up.
-                </div>
-            </div>
-        </div>
-        '; ?>
-    </div>
-
-    <div style="text-align: center; margin-top: 20px;">Redirecting in <span id="countdown">3</span></div>
-    <script>
-        function countdown() {
-            var i = 3;
-            var countdownElement = document.getElementById("countdown");
-            var countdownInterval = setInterval(function() {
-                i--;
-                countdownElement.textContent = i;
-                if (i <= 0) {
-                    clearInterval(countdownInterval);
-                    window.location.href = "../home/home.php";
-                }
-            }, 1000); // 1000 milliseconds = 1 second
-        }
-        window.onload = countdown;
-    </script>
+    <?php
+    header("location: ../home/home.php"); // Redirect to the home page
+    exit;
+?>
 </body>
 </html>
