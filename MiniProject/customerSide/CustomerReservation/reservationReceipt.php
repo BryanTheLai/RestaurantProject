6421f19fc14bfc18e0d2a7001a26411e4568a01b
PinkWhite <134790534+Brynlai@users.noreply.github.com>
2023-09-23 12:55:21 +0800

    Add files via upload

--- MiniProject/customerSide/CustomerReservation/reservationReceipt.php
+++ MiniProject/customerSide/CustomerReservation/reservationReceipt.php
@@ -24,14 +24,27 @@ if ($reservationInfo) {
     // Create a PDF using FPDF
     class PDF extends FPDF {
         function Header() {
+            // Set font and size for the logo text
             $this->SetFont('Arial', 'B', 20);
-            $this->Cell(0, 10, "Johnny's Restaurant", 0, 1, 'C');
-            $this->SetFont('Arial', 'B', 16);
-            $this->Ln();
-            $this->Cell(0, 10, 'Reservation Information', 1, 1, 'C');
+
+            // Create the logo text
+            $logoText = "JOHNNY'S DINING & BAR";
             
+            // Add a link-like style (you cannot create actual HTML links in PDF)
+            $this->SetTextColor(0, 0, 0); // Set the text color to blue
+            $this->Cell(0, 10, $logoText, 0, 1, 'C', false, '../home./home.php');
+
+            $this->SetTextColor(0); // Reset text color to default
+
+            // Add space
+            $this->Ln(10);
+
+            // Set font and size for "Reservation Information" text
+            $this->SetFont('Arial', 'B', 16);
+            $this->Cell(0, 10, 'Reservation Information', 1, 1, 'C');
         }
     }
+
 
     $pdf = new PDF();
     $pdf->AddPage();
@@ -64,8 +77,9 @@ if ($reservationInfo) {
     $pdf->Cell(40, 10, 'Special Request:', 1);
     $pdf->Cell(150, 10, $reservationInfo['special_request'], 1);
     $pdf->Ln();
-
-    $pdf->Output();
+    
+    
+    $pdf->Output('Reservation-Copy-ID' . $reservationInfo['reservation_id'] . '.pdf', 'D');
 } else {
     echo 'Invalid reservation ID or reservation not found.';
 }
