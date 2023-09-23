a597ec8e8c03d04c9f2ce854de5733f5b397619c
PinkWhite <134790534+Brynlai@users.noreply.github.com>
2023-09-23 12:03:59 +0800

    Update home.php

--- MiniProject/customerSide/home/home.php
+++ MiniProject/customerSide/home/home.php
@@ -164,8 +164,12 @@ mysqli_close($link);
 <!-- Hero Section with Video Background and Text Overlay -->
 <section id="hero" style="position: relative;">
     <video autoplay loop muted playsinline poster="your-poster-image.jpg" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover;">
-        <source src="../image/video (2160p).mp4" type="video/mp4">
-        <!-- Add additional source elements for other video formats if needed -->
+        <source src="../image/blackwhite.mp4" type="video/mp4">
+        <!-- Add additional source elements for 
+        1.  SteakOnGrillCloseup
+        2.  blackwhite
+        3.  fartongsgrill
+        other video formats if needed -->
     </video>
     <div class="hero container" style="position: relative; z-index: 1;">
         <div>
@@ -363,7 +367,7 @@ mysqli_close($link);
   <section id="footer">
     <div class="footer container">
         <div class="brand">
-          <h1>Want Drinks on Us?</h1>
+          <h1>Drinks on the House</h1>
       </div>
         <h2>Follow our Socials</h2>
       <div class="social-icon">
