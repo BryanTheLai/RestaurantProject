485e910e534e796104b24eb1c5db077d5e322a6c
yong <111368728+ahhyang@users.noreply.github.com>
2023-10-02 12:46:34 +0800

    Add files via upload

--- MiniProject/adminSide/panel/pos-panel.php
+++ MiniProject/adminSide/panel/pos-panel.php
@@ -1,25 +1,3 @@
 <?php
 session_start(); // Ensure session is started
-?>
-<?php
-include '../inc/dashHeader.php';
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <link href="../css/pos.css" rel="stylesheet" />
-    
-</head>
-<body>
-<div class="container">
-    <div id="POS-Content" class="row center-middle">
-            <a href="reservation-panel.php" style="width:20em;" class="btn btn-dark btn-lg m-5" title="View Details">Reservation</a>
-            <a href="../posBackend/posTable.php" style="width:20em;" class="btn btn-dark btn-lg m-5" title="View Details">Order</a>
-    </div>
-</div>
-
-<!-- Include footer -->
-<?php include '../inc/dashFooter.php' ?>
-</body>
-</html>
+header("location: ../posBackend/posTable.php");
