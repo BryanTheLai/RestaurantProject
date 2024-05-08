a4fb3937fb2843ab0ccc228039b37f7a37f79512
Edzer Dionido <jeddionido26@gmail.com>
2024-05-08 18:04:14 +0800

    remove whitespaces

---
+++ js/app.js
@@ -0,0 +1,30 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+const review_btn = document.querySelector('#review-btn');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+review_btn.addEventListener('click', () => {
+	location.href = "/customerSide/reviews/write-reviews.php";
+});
