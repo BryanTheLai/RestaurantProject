35c4318fcb787a0b005cd91f7971f857e0ebc1bf
Edzer Dionido <jeddionido26@gmail.com>
2024-05-16 16:21:47 +0800

    add a button

--- view/Customers/write-reviews.php
+++ view/Customers/write-reviews.php
@@ -1,14 +1,11 @@
 <?php 
-include_once('../components/header.php');
-// if(!isset($_SESSION['loggedin'])) {
-//     header("Location: /customerSide/customerLogin/login.php");
-// }
+include_once('view/components/header.php');
 ?>
 <section id="hero" style="position: relative;">
     <div class="hero container" style="position: relative; z-index: 1;">
        <div id="review-writing">
             <h1>Write us a review!</h1>
-            <form action="review_process.php" method="POST">
+            <form action="processes/Customers/review_process.php" method="POST">
                 <input type="hidden" name="action" value="review">
                 
                 <label for="title">Title</label>
@@ -20,11 +17,11 @@ include_once('../components/header.php');
                 <label for="ratings">Star Ratings</label>
                 <div class="rateYo" data-rateyo-full-star="true" data-rateyo-rating="0"></div>
                 <input type="hidden" name="rating">
-                <input type="submit" name="submit" value="Submit">
+                <input class="btn btn-primary" type="submit" name="submit" value="Submit">
             </form>
        </div>
     </div>
 </section>
 <?php 
-include_once('../components/footer.php');
+include_once('view/components/footer.php');
 ?>
