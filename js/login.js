1a1b04dc66457a4e67b7065b41af64681593f5a3
Edzer Dionido <jeddionido26@gmail.com>
2024-05-16 08:53:42 +0800

    fix validation

--- js/login.js
+++ js/login.js
@@ -4,6 +4,12 @@ $(document).ready(function () {
     $('#loginForm').submit(function (e) {
         e.preventDefault();
 
+        $('.err_msg').text(''); // Clear all error messages
+        $('.is-invalid').removeClass('is-invalid'); // Remove the invalid class from all elements
+        $('.main_error').text(''); // Clear the main error message
+
+        let formData;
+        
         $.ajax({
             type: "POST",
             url: "../processes/Customers/login-process.php",
@@ -16,26 +22,42 @@ $(document).ready(function () {
                 if (response.status === 'success') {
                     console.log("success!");
                     window.location.href = '/home'; // This should head back to home
-                } else if (response.status === 'failed' ){
+                } else if (response.status === 'failed'){
                     // console.error(response);
-                    for (const key in response.message) {
-                        if(key === "email") {
-                            console.log(key, response.message[key]);
-                        }
-
-                        if (key === "password"){
-                            console.log(key, response.message[key]);
-                        }
-
-                        if (key === "login_credentials"){
-                            console.log(key, response.message[key]);
-                        }
-                    }
+                    handleValidation(response.message);
+                    console.log("here");
                 } else {
-                    console.log("Error! There's an internal error happened");
+                    $('.main_error').text("There's an error occurred!");
                 }
             }
         });
-    })
+    });
+
+    function handleValidation(errors) {
+        
+        let credentialErrors = false; 
+        for (const key in errors) {
+            if (errors.hasOwnProperty(key)) {
+                if(key === "login_credentials") {
+                $('.main_error').text(errors[key]); // Display main error message
+                credentialErrors = true; // Set the flag to true
+                } else {
+                    let $input = $('[name="' + key + '"]');
+                    $input.addClass('is-invalid');
+                    $input.siblings('.err_msg').text(errors[key]);
+                }
+            }
+        }
+
+        if(!credentialErrors) {
+            $('.main_error').text('');
+        }
+    }
+
+    $('#loginForm input').on('input', function() {
+        var $input = $(this);
+        $input.removeClass('is-invalid');
+        $input.siblings('.err_msg').text('');
+    });
 
 });
